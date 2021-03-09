29d246683fa208543967b8b1ac6c36f94ae48a00
admirkrilasevic <admirkrilasevic@gmail.com>
2021-03-09 14:27:45 +0100

    config change

--- api/dao/BaseDao.class.php
+++ api/dao/BaseDao.class.php
@@ -3,15 +3,14 @@
 require_once dirname(__FILE__)."/../config.php";
 class BaseDao
 {
+  private $connection;
 
   function __construct(){
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
+
 
     try {
-      $conn = new PDO("mysql:host=".Config::DB_HOST."dbname=".Config::DB_SCHEME, Config::DB_USERNAME, Config::DB_PASSWORD);
-      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $this->$connection = new PDO("mysql:host=".Config::DB_HOST.;"dbname=".Config::DB_SCHEME, Config::DB_USERNAME, Config::DB_PASSWORD);
+      $this->$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       echo "Connected successfully";
     } catch(PDOException $e) {
       echo "Connection failed: " . $e->getMessage();
