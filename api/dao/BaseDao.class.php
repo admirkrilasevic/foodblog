948fc092d076ce66b363b28131d117cb570fe207
admirkrilasevic <admirkrilasevic@gmail.com>
2021-03-09 14:29:01 +0100

    config change

--- api/dao/BaseDao.class.php
+++ api/dao/BaseDao.class.php
@@ -9,7 +9,7 @@ class BaseDao
 
 
     try {
-      $this->$connection = new PDO("mysql:host=".Config::DB_HOST.;"dbname=".Config::DB_SCHEME, Config::DB_USERNAME, Config::DB_PASSWORD);
+      $this->$connection = new PDO("mysql:host=".Config::DB_HOST.";dbname=".Config::DB_SCHEME, Config::DB_USERNAME, Config::DB_PASSWORD);
       $this->$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       echo "Connected successfully";
     } catch(PDOException $e) {
