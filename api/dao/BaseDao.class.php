77eb9b09406d64b440e04214378bb4534050e547
admirkrilasevic <admirkrilasevic@gmail.com>
2021-03-10 11:17:18 +0100

    added update and queries

--- api/dao/BaseDao.class.php
+++ api/dao/BaseDao.class.php
@@ -22,16 +22,28 @@ class BaseDao
 
   }
 
-  function update(){
+  public function update($table, $id, $entity, $id_column = "id"){
+    $query = "UPDATE ${table} SET ";
+    foreach($entity as $name => $value){
+      $query .= $name ."= :". $name. ", ";
+    }
+    $query = substr($query, 0, -2);
+    $query .= " WHERE ${id_column} = :id";
 
+    $stmt= $this->connection->prepare($query);
+    $entity['id'] = $id;
+    $stmt->execute($entity);
   }
 
-  function query(){
-
+  public function query($query, $params){
+    $stmt = $this->connection->prepare($query);
+    $stmt->execute($params);
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
   }
 
-  function query_unique(){
-
+  public function query_unique($query, $params){
+    $results = $this->query($query, $params);
+    return reset($results);
   }
 
 }
