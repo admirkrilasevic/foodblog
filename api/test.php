19e6006d4652d65b075c4216d4c0d7f769e189d4
admirkrilasevic <admirkrilasevic@gmail.com>
2021-03-10 11:20:56 +0100

    added test.php

---
+++ api/test.php
@@ -0,0 +1,23 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once dirname(__FILE__). "/dao/UserDao.class.php";
+
+$user_dao = new UserDao();
+
+//$user = $user_dao->get_user_by_id(3);
+
+$user1 = [
+  "password" => "123"
+  "name" => "Admir Krilasevic"
+];
+
+$user = $user_dao->update_user_by_email("fare@stu.ibu.edu.ba", $user1);
+
+//$user_dao->get_user_by_email("dino.keco@gmail.com");
+
+print_r($user);
+
+?>
