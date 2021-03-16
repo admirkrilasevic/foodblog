d5eff4d3802dafa0b06ab62f8133e850f736acd5
admirkrilasevic <admirkrilasevic@gmail.com>
2021-03-16 14:11:11 +0100

    correction

--- api/dao/BaseDao.class.php
+++ api/dao/BaseDao.class.php
@@ -15,7 +15,7 @@ class BaseDao
       $this->connection = new PDO("mysql:host=".Config::DB_HOST.";dbname=".Config::DB_SCHEME, Config::DB_USERNAME, Config::DB_PASSWORD);
       $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e) {
-      thow $e;
+      throw $e;
     }
 
   }
