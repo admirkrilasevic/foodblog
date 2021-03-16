c0cded597e9200bbe9aa47bbf9259e7b95db4090
admirkrilasevic <admirkrilasevic@gmail.com>
2021-03-16 15:31:20 +0100

    initial flight setup

--- api/index.php
+++ api/index.php
@@ -1,7 +1,12 @@
 <?php
 
+require dirname(__FILE__).'/../vendor/autoload.php';
 require_once dirname(__FILE__)."/dao/BaseDao.class.php";
-$mydb = new BaseDao();
-echo "Hello from API";
+
+Flight::route('/', function(){
+    echo 'hello world!';
+});
+
+Flight::start();
 
  ?>
