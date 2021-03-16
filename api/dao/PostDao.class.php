57b043122002eac8b19f4c4bdaba3a8d2a461473
admirkrilasevic <admirkrilasevic@gmail.com>
2021-03-16 13:59:14 +0100

    added all dao classes with constructors

---
+++ api/dao/PostDao.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once dirname(__FILE__).".../BaseDao.class.php"
+
+public class PostDao extends BaseDao{
+
+  public __construct(){
+    parent::__construct("posts");
+  }
+}
+
+ ?>
