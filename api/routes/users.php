92b0ca96cbc4d7d566c8e2c63f407f144a2a4e76
admirkrilasevic <admirkrilasevic@gmail.com>
2021-03-27 00:19:38 +0100

    added comments

--- api/routes/users.php
+++ api/routes/users.php
@@ -1,14 +1,18 @@
 <?php
 
+//register user
 Flight::route('POST /users/register', function(){
   $data = Flight::request()->data->getData();
   Flight::json(Flight::userService()->register($data));
 });
 
+//confirm registration
 Flight::route('GET /users/confirm/@token', function($token){
   Flight::userService()->confirm($token);
   Flight::json(["message" => "Your account has been activated"]);
+});
 
+//get users
 Flight::route('GET /users', function(){
   $offset = Flight::query('offset', 0);
   $limit = Flight::query('limit', 25);
@@ -18,19 +22,22 @@ Flight::route('GET /users', function(){
   Flight::json(Flight::userService()->get_users($search, $offset, $limit, $order));
 });
 
+//get user by id
 Flight::route('GET /users/@id', function($id){
   Flight::json(Flight::userService()->get_by_id($id));
 });
 
+//add user
 Flight::route('POST /users', function(){
   $data = Flight::request()->data->getData();
   Flight::json(Flight::userService()->add($data));
 });
 
+//edit user by id
 Flight::route('PUT /users/@id', function($id){
   $data = Flight::request()->data->getData();
   Flight::json(Flight::userService()->update($id, $data));
 });
-});
+
 
 ?>
