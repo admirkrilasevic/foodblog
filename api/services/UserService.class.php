7d55841a5673780bd48439b594f953e6e0e56c8a
admirkrilasevic <admirkrilasevic@gmail.com>
2021-03-29 00:39:54 +0200

    added login-related functionalities with email confirmation

--- api/services/UserService.class.php
+++ api/services/UserService.class.php
@@ -2,11 +2,15 @@
 
 require_once dirname(__FILE__). '/BaseService.class.php';
 require_once dirname(__FILE__).'/../dao/UserDao.class.php';
+require_once dirname(__FILE__).'/../clients/SMTPClient.class.php';
 
 class UserService extends BaseService{
 
+  private $smtpClient;
+
   public function __construct(){
     $this->dao = new UserDao();
+    $this->smtpClient = new SMTPClient();
   }
 
   public function get_users($search, $offset, $limit, $order){
@@ -26,7 +30,7 @@ class UserService extends BaseService{
         "name" => $user['name'],
         "username" => $user['username'],
         "email" => $user['email'],
-        "password" => $user['password'],
+        "password" => md5($user['password']),
         "admin" => "0",
         "profile_picture" => null,
         "status" => "pending",
@@ -45,22 +49,38 @@ class UserService extends BaseService{
       }
     }
 
-
-    // TODO: send email with some token
+    $this->smtpClient->send_register_user_token($user);
 
     return $user;
   }
 
   public function confirm($token){
     $user = $this->dao->get_user_by_token($token);
-
     if (!isset($user['id'])) throw Exception("Invalid token");
-
     $this->dao->update($user['id'], ["status" => "ACTIVE"]);
-
-    //TODO send email to customer
+    $this->accountDao->update($user['account_id'], ["status" => "ACTIVE"]);
   }
 
+  public function login($user){
+    $db_user = $this->dao->get_user_by_email($user["email"]);
+    if(!isset($db_user["id"])) throw new Exception("User doesn't exist",400);
+    if($db_user["status"] != "ACTIVE") throw new Exception("Account not active",400);
+    if($db_user["password"] != md5($user["password"])) throw new Exception("Invalid password",400);
+    return $db_user;
+  }
+
+  public function reset($user){
+    $db_user = $this->dao->get_user_by_token($user["token"]);
+    if(!isset($db_user["id"])) throw new Exception("Invalid token",400);
+    $this->dao->update($db_user["id"],["password"=>md5($user["password"])]);
+  }
+
+  public function forgot($user){
+    $db_user = $this->dao->get_user_by_email($user["email"]);
+    if(!isset($db_user["id"])) throw new Exception("User doesn't exist",400);
+    $db_user = $this->update($db_user["id"],["token" => md5(random_bytes(16))]);
+    $this->smtpClient->send_user_recovery_token($db_user);
+  }
 
 }
 ?>
