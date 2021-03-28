e3e3a1178c9ca9ba673818453f9b0d124604e343
admirkrilasevic <admirkrilasevic@gmail.com>
2021-03-29 00:11:24 +0200

    removed POST/users route

--- api/routes/users.php
+++ api/routes/users.php
@@ -68,27 +68,6 @@ Flight::route('GET /users/@id', function($id){
 });
 
 /**
- * @OA\Post(path="/users", tags={"user"},
- *   @OA\RequestBody(description="Basic user info", required=true,
- *       @OA\MediaType(mediaType="application/json",
- *    			@OA\Schema(
- *    				 @OA\Property(property="name", required="true", type="string", example="My Test Account",	description="Name of the user" ),
- *    				 @OA\Property(property="username", required="true", type="string", example="testaccount",	description="Username of the user" ),
- *    				 @OA\Property(property="email", required="true", type="string", example="test@account.com",	description="Email of the user" ),
- *    				 @OA\Property(property="password", required="true", type="string", example="testaccount123",	description="Password of the user" ),
- *    				 @OA\Property(property="created_at", required="true", type="datetime", example="2021-03-28 23:33:00",	description="Time user is created" )
- *          )
- *       )
- *     ),
- *  @OA\Response(response="200", description="User that has been added into database with ID assigned.")
- * )
- */
-Flight::route('POST /users', function(){
-  $data = Flight::request()->data->getData();
-  Flight::json(Flight::userService()->add($data));
-});
-
-/**
  * @OA\Put(path="/users/{id}", tags={"user"},
  *   @OA\Parameter(@OA\Schema(type="integer"), in="path", name="id", default=1),
  *   @OA\RequestBody(description="Basic user info that is going to be updated", required=true,
