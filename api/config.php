2e6a53eed15458c5f605560ea8c4e0134c130fde
admirkrilasevic <admirkrilasevic@gmail.com>
2021-04-07 16:47:30 +0200

    set environment variables

--- api/config.php
+++ api/config.php
@@ -1,10 +1,21 @@
 <?php
 class Config{
   const DATE_FORMAT = "Y-m-d H:i:s";
-  const DB_HOST = "localhost";
-  const DB_USERNAME = "root";
-  const DB_PASSWORD = "";
-  const DB_SCHEME = "foodblog";
+  public static function DB_HOST(){
+    return Config::get_env("DB_HOST", "localhost");
+  }
+  public static function DB_USERNAME(){
+    return Config::get_env("DB_USERNAME", "root");
+  }
+  public static function DB_PASSWORD(){
+    return Config::get_env("DB_PASSWORD", "");
+  }
+  public static function DB_SCHEME(){
+    return Config::get_env("DB_SCHEME", "foodblog");
+  }
+  public static function DB_PORT(){
+    return Config::get_env("DB_PORT", "3306");
+  }
 
   const SMTP_HOST = "smtp.gmail.com";
   const SMTP_PORT = 587;
@@ -14,6 +25,10 @@ class Config{
 
   const JWT_SECRET = "nbf3095zn64";
   const JWT_TOKEN_TIME = 604800;
+
+  public static function get_env($name, $default){
+  return isset($_ENV[$name]) && trim($_ENV[$name]) != '' ? $_ENV[$name] : $default;
+}
 }
 
  ?>
