5218d66c25003da10dd6ff8118dd174725091bbe
admirkrilasevic <admirkrilasevic@gmail.com>
2021-04-12 11:37:10 +0200

    smtp fix

--- api/clients/SMTPClient.class.php
+++ api/clients/SMTPClient.class.php
@@ -8,7 +8,7 @@ class SMTPClient {
 
 
   public function __construct(){
-    $transport = (new Swift_SmtpTransport(Config::SMTP_HOST(), Config::SMTP_PORT(), Config::SMTP_ENCRYPT()))
+    $transport = (new Swift_SmtpTransport(Config::SMTP_HOST(), Config::SMTP_PORT(), 'tls'))
       ->setUsername(Config::SMTP_USER())
       ->setPassword(Config::SMTP_PASSWORD());
 
